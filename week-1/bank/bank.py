6aca5b2c188a0a22b6a99a09ebf6d87866fcd8d8
Olive1r4 <fernandooliveira@live.com>
2024-10-29 19:35:05 -0300

    bank

--- week-1/bank/bank.py
+++ week-1/bank/bank.py
@@ -1,6 +1,13 @@
 def main():
     n = input("Greetings: ").lower()
-    print(n[0:1])
+    valortopay(n)
 
+def valortopay(valor):
+    if valor[0:5] == "hello":
+        print("$0")
+    elif valor[0] == "h" and valor[1:5] != "ello":
+        print("$20")
+    else:
+        print("$100")
 if __name__ == "__main__":
     main()
